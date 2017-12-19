dc175b34cca52e9823d8e417438610e0611f1300
Andres Encarnacion Ortiz <aencarnacion@nt.core>
2017-12-19 17:55:52 -0400

    New improvements on the form handling.

--- src/MovementForm.js
+++ src/MovementForm.js
@@ -7,41 +7,68 @@ class MovementForm extends Component {
     super(props);
 
     // Binding Methods to Current Context
-    this.toggleLanguage = this.toggleLanguage.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.setDepth = this.setDepth.bind(this);
     
     // State Primary Variables
-    this.state = {
-      header: 'Hola',
-      date: new Date(),
-      movements: ([{id: 1, description: 'Went to McDonalds', amount: 675}]).map((m) => {
-        return <p key={m.id}> I {m.description} and paid {m.amount} </p>
-      })
-    }
+    this.state = {};
+
+    // Current Date (Updates automatically)
+    this.state.date = new Date();
+
+    this.movement = {
+      description: '',
+      amount: 0
+    };
+
+    // List of Movements
+    this.state.movements =  ([{id: 1, description: 'Went to McDonalds', amount: 675}]).map((m) => {
+      return <p key={m.id}> I {m.description} and paid {m.amount} </p>
+    });
+    
   }
 
+  // On Component Init
   componentDidMount() {
-    this.timer = setInterval( () => this.tick(), 1000)
+    this.timer = setInterval( () => this.tick(), 1000);
   }
 
+  // On Component Destroy
   componentWillUnmount() {
     clearInterval(this.timer);
   }
 
+  handleChange(event) {
+    let _state = this.setDepth({}, event.target.name, event.target.value);
+    this.setState(_state);
+    console.log(this.state);
+  }
+
+  // Updates the current Time
   tick() {
     this.setState({
       date: new Date()
     });
   }
 
+  // Add a Movement into the Movement List
   addMovement(e) {
     e.preventDefault();
     alert('Yai!');
   }
 
-  toggleLanguage(e) {
-    this.setState((prevState, props) => ({
-      header: (prevState.header === 'Hello') ? 'Hola' : 'Hello'
-    }));
+  // Set values to nested and non nested variables
+  setDepth(obj, path, value) {
+    obj = obj || {};
+    let tags = path.split(".").reverse();
+
+    obj[tags.shift()] = value;
+
+    for (let tag of tags) {
+      let _obj = Object.assign({}, obj);
+      obj = {[tag]: _obj};
+    }
+    return obj;
   }
 
   render() {
@@ -50,9 +77,9 @@ class MovementForm extends Component {
         <h1> {this.state.header} {this.props.name}</h1>
         <h2> It is {this.state.date.toLocaleTimeString()}. What did you do today?</h2>
         <form>
-          <input type="text" name="description" id="description" placeholder="Description"/>
+          <input type="text" id="description" name="movement.description" placeholder="Description" onChange={this.handleChange}/>
           <br/>
-          <input type="number" name="amount" id="amount" placeholder="Amount"/>
+          <input type="number" id="amount" name="movement.amount" placeholder="Amount" onChange={this.handleChange}/>
           <br/>
           <button type="submit" onClick={this.addMovement}> Save </button> &nbsp;
           <button type="button" onClick={this.toggleLanguage}> Change Language </button>
