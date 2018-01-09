02928d58199dca0428a4371a534c1f21e66b60e2
Andres Encarnacion Ortiz <aencarnacion@nt.core>
2018-01-09 17:49:01 -0400

    - Updated Readme.MD - Started with the navbar

--- src/App.js
+++ src/App.js
@@ -2,32 +2,25 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 
-
-// import logo from './logo.svg';
-import './App.css';
-
-// import MovementForm from './MovementForm.js'
+import NavBar from './NavBar'
 import FuelContainer from './Fuel'
-// import BackButton from './BackButton';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        
-        <div className="row">
-            
-          <div className="col">
-            
-            <FuelContainer />
-
-          </div>
-
-        </div>
-
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <NavBar/>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <FuelContainer />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import './App.css';
+// import logo from './logo.svg';
+
+class NavBar extends Component {
+
+    render = () => (
+        <nav class="navbar avbar-dark bg-dark">
+            <a class="navbar-brand">
+                Bootstrap
+            </a>
+        </nav>
+    )
+        
+}
+
+export default NavBar;
